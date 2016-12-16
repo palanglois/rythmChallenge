70f62751875c5c8933513f3c6fc3e6950bcf3ced
palanglois <thefroggy27@gmail.com>
2016-12-16 16:40:49 +0100

    Adding functionnality to load the data and perform a PLS on it

---
+++ loadData.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sklearn
+from matplotlib.mlab import PCA
+from sklearn.cross_decomposition import PLSRegression
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("train_input.csv", sep=';')
+label = pd.read_csv("train_output.csv", sep=';')
+
+data2 = data.iloc[0:-1,0:-2]
+label2 = label.iloc[0:-1,1]
+
+
