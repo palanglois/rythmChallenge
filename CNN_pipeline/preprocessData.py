311a3a19c8bc16cc592ecbdbb0715ef820227400
palanglois <thefroggy27@gmail.com>
2017-02-23 18:26:16 +0100

    Good version of the NN

--- CNN_pipeline/preprocessData.py
+++ CNN_pipeline/preprocessData.py
@@ -15,7 +15,7 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
 
 
 #n is the size of one subsample
-def augmentData(x,y,n):
+def augmentData(x,y,n,lowFreq,highFreq):
   nbData = x.shape[0]
   dimData = x.shape[1]
   #For one data, we make nFactor data
@@ -24,10 +24,17 @@ def augmentData(x,y,n):
   newY = []
   for i in range(nbData):
     for j in range(nFactor):
-      subX = x[i,j*n:(j+1)*n]
+      subX = np.log(np.absolute(np.fft.fft(x[i,j*n:(j+1)*n])))
       subY = y[i]
+      freqs = np.fft.fftfreq(len(subX))*250
+      ind = np.where(np.logical_and(freqs<highFreq,freqs>lowFreq))
+      subX = subX[ind]
       newX.append(subX)
       newY.append(subY)
   return np.array(newX), np.array(newY)
 
-
+def normalizeData(x):
+  xMean = np.mean(x,0)
+  xDev = np.std(x,0)
+  xDev[xDev == 0] = 1
+  return np.divide((x - xMean), xDev)
