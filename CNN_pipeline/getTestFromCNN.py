c62c0b6c6619b1d2ac3dcd2aee8a75204af65228
Chaimaa Kadaoui <chaimaa.kadaoui@gmail.com>
2017-02-24 15:29:11 +0100

    Modified function to aggregate test results

--- CNN_pipeline/getTestFromCNN.py
+++ CNN_pipeline/getTestFromCNN.py
@@ -15,6 +15,31 @@ def getTestFromCNN(cnn,csvName):
   np.savetxt(csvName, np.transpose(yReal), delimiter=",",fmt='%1.0f')
 
 
+def getTestFromCNNWithAgg(cnn, csvName, type_agg="mean"):
+  data = pd.read_csv("data/test_input.csv", sep=';')
+  npData = data.iloc[:,2:-1].values
+  xTest_fil = butter_bandpass_filter(npData, lowFreq, highFreq, sampling_freq, order=butter_order)
+  fakeY = np.zeros((npData.shape[0],dimOutput))
+  xTestFinal, yTestFinal = augmentData(xTest_fil,fakeY,sizeSeg,lowFreq,highFreq)
+  xTestFinal = normalizeData(xTestFinal)
+  yTestAges = np.argmax(cnn.y.eval(feed_dict={ cnn.x:xTestFinal, cnn.keep_prob:1.0}),1)
+  dimData = xTest_fil.shape[1]
+  nFactor = dimData // sizeSeg
+  ids = data.iloc[:,0].values
+  newIds = np.repeat(ids, dimData//sizeSeg)
+  yTestAges = pd.DataFrame({"ID": newIds, "TARGET":yTestAges})
+  if type_agg=="mean":
+    yReal = yTestAges.groupby("ID").mean()
+  elif type_agg=="max":
+    yReal = yTestAges.groupby("ID").max()
+  elif type_agg=="min":
+    yReal = yTestAges.groupby("ID").min()
+  else:
+    print "Wrong aggregation name"
+  yReal = yReal.apply(np.round)
+  yReal.to_csv("test_output.csv", sep=";")
+
+
 def averageFun(x,n):
   y = []
   for i in range(len(x)//n):
